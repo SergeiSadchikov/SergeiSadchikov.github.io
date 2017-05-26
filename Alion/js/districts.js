cc6973e0b1d5886e4fbd527a9da892dec343f7c2
SergeiSadchikov <sadchikov.s@gmail.com>
2017-05-26 21:57:31 +0400

    adding Alion demo

---
+++ Alion/js/districts.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    $("input[type='radio']").on('change', function () {
+        $('.districts-block__item').each(function () {
+            if ($(this).css('max-height') === '500px') {
+                $(this).css('max-height', '0px');
+            } else {
+                $(this).css('max-height', '500px');
+            }
+
+        });
+        $('.close-button').toggleClass('close-button-hidden');
+        $('.district-block__info').each(function () {
+            if (!($(this).hasClass('hidden-block'))) {
+                $(this).addClass('hidden-block');
+            }
+        });
+        $('.district-block__info[data-value=' + $(this).attr('value') + ']').toggleClass('hidden-block');
+    });
+}());
